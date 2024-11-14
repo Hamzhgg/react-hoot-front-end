0db7ddec4a2f9f26b64345ba9f766e8ab9a72c7d
Hamzhgg <gharibehhamzh@gmail.com>
2024-11-14 14:28:11 +0300

    all

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,48 @@
 import { Link } from 'react-router-dom';
+import { AuthedUserContext } from '../../App';
+import { useContext } from 'react';
+import styles from './NavBar.module.css'
+import Logo from '../../assets/images/logo.svg'
 
-const NavBar = ({ user }) => {
+const NavBar = ({ handleSignout }) => {
+  const user = useContext(AuthedUserContext);
   return (
     <>
-      { user ? (
-        <nav>
+      {user ? (
+        <nav className={styles.container}>
+          <Link to='/'><img src={Logo} /></Link>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="">Sign Out</Link></li>
+            <li>Welcome, {user.username}</li>
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to='/hoots'>HOOTS</Link>
+            </li>
+            <li>
+              <Link to='/hoots/new'>NEW HOOT</Link>
+            </li>
+            <li>
+              <Link to="" onClick={handleSignout}>
+                Sign Out
+              </Link>
+            </li>
           </ul>
         </nav>
       ) : (
-        <nav>
+        <nav className={styles.container}>
+          <Link to='/'><img src={Logo} /></Link>
           <ul>
-            <li><Link to="/signin">Sign In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
           </ul>
         </nav>
       )}
     </>
-  )
-}
-
+  );
+};
 export default NavBar;
